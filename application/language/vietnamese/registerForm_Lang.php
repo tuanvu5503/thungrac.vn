424b376bdbe7c800c794c289ea2dc4abef517a3f
Tuan Vu <vu1111482@gmail.com>
2015-12-21 13:24:26 +0700

    ok

---
+++ application/language/vietnamese/registerForm_Lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang['legend_register'] = "Đăng ký tài khoản";
+
+$lang['username'] = "Tên đăng nhập";
+$lang['email'] = "Hòm thư";
+$lang['password'] = "Mật khẩu";
+$lang['repassword'] = "Xác nhận mật khẩu";
+
+$lang['btnSubmit'] = "Đăng ký";
+$lang['holderUsername'] = "Nhập tên đăng nhập";
+$lang['holderPassword'] = "Nhập mật khẩu";
+$lang['holderEmail'] = "Nhập địa chỉ email";
+$lang['holderRepassword'] = "Nhập lại mật khẩu";
+
+?>
