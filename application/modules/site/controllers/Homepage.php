bbbe743a8e2637dabae052d161282ccabac3c7ff
Tuan Vu <vu1111482@gmail.com>
2015-12-21 11:53:28 +0700

    Merge branch '2' (reverted from commit b1dc450b61d6b9836f314536274e293bd1b53a2f)

---
+++ application/modules/site/controllers/Homepage.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Homepage extends MX_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Productmodel');
+    }
+    public function index()
+    {
+        $this->load->helper('convert');
+        $all_superCategory = $this->Productmodel->all_superCategory();
+
+        $products['SẢN PHẨM MỚI'] = $this->Productmodel->new_product();;
+        foreach ($all_superCategory as $row) {
+            $products[$row['super_categoryName']] = $this->Productmodel->listProductbySuperId($row['id']);
+            $menus[$row['super_categoryName']] = $this->Productmodel->getMenu($row['id']);
+        }
+
+        $data['products'] = $products;
+        $data['menus'] = $menus;
+        
+        $data['title'] = "Kinh doanh thùng rác";
+        $data['subView'] = "allProduct_layout";
+        $data['menu'] = $this->Productmodel->listCategory();
+        $data['subData'] = $data;
+
+        $this->load->view('main_layout', $data);
+    }
+
+    public function view_detail()
+    {
+        if ($this->uri->segment(5))
+        {
+            $id = explode('-', $this->uri->segment(5));
+            $id = (int) end($id);
+            
+            $category_id=$this->Productmodel->get_categoryid_by_productid($id);
+            $data['relative_product'] = $this->Productmodel->get_categorybyid($category_id);
+            $data['info'] = $this->Productmodel->find_productbyid($id);
+            if (count($data['info']) == 0)
+            {
+                $data['title'] = "View product";
+                $data['error'] = "Không tìm thấy sản phẩm";
+                $this->load->view('/product/error', $data);
+                die;
+            }
+            $all_superCategory = $this->Productmodel->all_superCategory();
+
+            foreach ($all_superCategory as $row) {
+                $menus[$row['super_categoryName']] = $this->Productmodel->getMenu($row['id']);
+            }
+
+            $data['menus'] = $menus;
+            $data['menu'] = $this->Productmodel->listCategory();
+            
+            $data['title'] = "View product";
+            $this->load->view('/product/view_detail', $data);
+        } else {
+            $this->load->view('404-temp');
+        }
+    }
+}
