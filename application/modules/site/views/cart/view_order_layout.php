1aa2b5c65ba514b10b32df653c825341981a33b6
Tuan Vu <vu1111482@gmail.com>
2015-12-28 14:26:30 +0700

    dsad

--- application/modules/site/views/cart/view_order_layout.php
+++ application/modules/site/views/cart/view_order_layout.php
@@ -2,98 +2,73 @@
 	<link href="<?php echo base_url().'public/css/site/cart/normalize.css' ?>" rel="stylesheet">
 	<link href="<?php echo base_url().'public/css/site/cart/style.css' ?>" rel="stylesheet">
 	
-    
-    <h1>Đơn đặt hàng</h1>
 
-<div class="shopping-cart2">
+  <h1>Đơn đặt hàng</h1>
 
-  <div class="column-labels">
-    <label class="product-image">Hình ảnh</label>
-    <label class="product-details">Sản phẩm</label>
-    <label class="product-price">Giá</label>
-    <label class="product-quantity">Số lượng</label>
-    <label class="product-removal">Thao tác</label>
-    <label class="product-line-price">Tổng tiền</label>
+  <div class="shopping-cart2">
+
+    <div class="column-labels">
+      <label class="product-image">Hình ảnh</label>
+      <label class="product-details">Sản phẩm</label>
+      <label class="product-price">Giá</label>
+      <label class="product-quantity">Số lượng</label>
+      <label class="product-removal">Thao tác</label>
+      <label class="product-line-price">Thành tiền</label>
+    </div>
+
+    <form action="" method="POST" role="form">
+        <?php 
+        $total = 0;
+        foreach ($info_of_order_product_array as $items) {
+          $total += $items['price_x_order_qty'];
+          ?>
+          <div class="product">
+            <div class="product-image">
+            <img src="<?php echo base_url().'public/img/products/'.$items['image']; ?>">
+            </div>
+            <div class="product-details">
+              <div class="product-title"><?php echo $items['product_name']; ?></div>
+              <p class="product-description"><?php echo $items['des']; ?></p>
+            </div>
+            <div class="product-price"><?php echo number_format($items['price']); ?></div>
+            <div class="product-quantity">
+              <input type="number" value="<?php echo $items['order_qty']; ?>" min="1">
+            </div>
+            <div class="product-removal">
+              <button type="button" class="remove-product">
+                Xóa
+              </button>
+            </div>
+            <div class="product-line-price"><?php echo number_format($items['price_x_order_qty']); ?></div>
+          </div>
+
+          <?php
+        }
+        ?>
+    </form>
+
+    <div class="totals">
+     <!--  <div class="totals-item">
+        <label>Subtotal</label>
+        <div class="totals-value" id="cart-subtotal">71.97</div>
+      </div>
+      <div class="totals-item">
+        <label>Tax (5%)</label>
+        <div class="totals-value" id="cart-tax">3.60</div>
+      </div>
+      <div class="totals-item">
+        <label>Shipping</label>
+        <div class="totals-value" id="cart-shipping">15.00</div>
+      </div> -->
+      <div class="totals-item totals-item-total">
+        <label>Tổng cộng</label>
+        <div class="totals-value" id="cart-total"><?php echo number_format($total); ?></div>
+      </div>
+    </div>
+
+    <button class="checkout">Đặt hàng</button>
+
   </div>
-
-
-
-
-
-
-
-
-  
-
-  <div class="product">
-    <div class="product-image">
-      <img src="http://s.cdpn.io/3/dingo-dog-bones.jpg">
-    </div>
-    <div class="product-details">
-      <div class="product-title">Dingo Dog Bones</div>
-      <p class="product-description">The best dog bones of all time. Holy crap. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.</p>
-    </div>
-    <div class="product-price">12.99</div>
-    <div class="product-quantity">
-      <input type="number" value="2" min="1">
-    </div>
-    <div class="product-removal">
-      <button class="remove-product">
-        Xóa
-      </button>
-    </div>
-    <div class="product-line-price">25.98</div>
-  </div>
-
-
-
-
-
-  <div class="product">
-    <div class="product-image">
-      <img src="http://s.cdpn.io/3/large-NutroNaturalChoiceAdultLambMealandRiceDryDogFood.png">
-    </div>
-    <div class="product-details">
-      <div class="product-title">Nutro™ Adult Lamb and Rice Dog Food</div>
-      <p class="product-description">Who doesn't like lamb and rice? We've all hit the halal cart at 3am while quasi-blackout after a night of binge drinking in Manhattan. Now it's your dog's turn!</p>
-    </div>
-    <div class="product-price">45.99</div>
-    <div class="product-quantity">
-      <input type="number" value="1" min="1">
-    </div>
-    <div class="product-removal">
-      <button class="remove-product">
-        Xóa
-      </button>
-    </div>
-    <div class="product-line-price">45.99</div>
-  </div>
-
-
-
-
-  <div class="totals">
-    <div class="totals-item">
-      <label>Subtotal</label>
-      <div class="totals-value" id="cart-subtotal">71.97</div>
-    </div>
-    <div class="totals-item">
-      <label>Tax (5%)</label>
-      <div class="totals-value" id="cart-tax">3.60</div>
-    </div>
-    <div class="totals-item">
-      <label>Shipping</label>
-      <div class="totals-value" id="cart-shipping">15.00</div>
-    </div>
-    <div class="totals-item totals-item-total">
-      <label>Grand Total</label>
-      <div class="totals-value" id="cart-total">90.57</div>
-    </div>
-  </div>
-      
-      <button class="checkout">Checkout</button>
 
 </div>
-
-</div>
-	<script src="<?php echo base_url().'public/js/site/cart/index.js'?>"></script>
+<script src="<?php echo base_url().'public/js/site/cart/index.js'?>"></script>
